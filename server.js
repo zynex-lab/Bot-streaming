b29ad95629cf1079e3e87e3445843c5e8f4a6374
zynex <113895823+zynex-lab@users.noreply.github.com>
2025-02-26 02:18:03 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const app = express();
 
+const PORT = process.env.PORT || 3000; // ใช้ PORT ของ Render
+
 app.get('/', (req, res) => {
   res.send('Selfbot is running!');
 });
 
-app.listen(3000, () => {
-  console.log('✅ Server is running on port 3000');
+app.listen(PORT, () => {
+  console.log(`✅ Server is running on port ${PORT}`);
 });
 
 module.exports = () => {
